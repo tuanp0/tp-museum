c1c3de526a8de0255c9ba5375772d9bbe2f91c47
tuanph0 <admin@tuanphung.com>
2025-11-12 21:18:09 +0100

    Intro WIP

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, {useState, useEffect} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Container from '@/components/Container'
@@ -6,26 +7,54 @@ import Container from '@/components/Container'
 import styles from './Header.module.scss'
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      if (window.scrollY > 200) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isScrolled && styles.scrolled}`}>
         <Container className={styles.container} noPadding>
           <h1 className={styles.headerTitle}>
             <Image src={`./tp-museum.svg`} alt={`TP Museum | Musée - Librairie - Café`} width={280} height={78} className={styles.headerLogo} />
             <span className={styles.headerSpan}>Tp Museum</span>
           </h1>
-          <div className={styles.headerRs}>
-            <Link href={`#`} title={`X Twitter`} className={styles.headerRsItem}>
-              <Image src={`./x.svg`} alt={`X Twitter`} width={280} height={78} className={styles.headerRsX} />
-            </Link>
-            <Link href={`#`} title={`Instagram`} className={styles.headerRsItem}>
-              <Image src={`./ig.svg`} alt={`Instagram`} width={280} height={78} className={styles.headerRsIg} />
-            </Link>
-            <Link href={`#`} title={`Facebook`} className={styles.headerRsItem}>
-              <Image src={`./fb.svg`} alt={`Facebook`} width={280} height={78} className={styles.headerRsFb} />
-            </Link>
-            <Link href={`#`} title={`YouTube`} className={styles.headerRsItem}>
-              <Image src={`./yt.svg`} alt={`YouTube`} width={280} height={78} className={styles.headerRsYt} />
-            </Link>
+          <div className={styles.headerLinks}>
+            <div className={styles.headerRs}>
+              <Link href={`#`} title={`X Twitter`} className={styles.headerRsItem}>
+                <Image src={`./x.svg`} alt={`X Twitter`} width={280} height={78} className={styles.headerRsX} />
+              </Link>
+              <Link href={`#`} title={`Instagram`} className={styles.headerRsItem}>
+                <Image src={`./ig.svg`} alt={`Instagram`} width={280} height={78} className={styles.headerRsIg} />
+              </Link>
+              <Link href={`#`} title={`Facebook`} className={styles.headerRsItem}>
+                <Image src={`./fb.svg`} alt={`Facebook`} width={280} height={78} className={styles.headerRsFb} />
+              </Link>
+              <Link href={`#`} title={`YouTube`} className={styles.headerRsItem}>
+                <Image src={`./yt.svg`} alt={`YouTube`} width={280} height={78} className={styles.headerRsYt} />
+              </Link>
+            </div>
+            <nav className={styles.headerMenu}>
+              <ul className={styles.headerMenuUl}>
+                <li className={styles.headerMenuLi}><Link href={`#`} title={``} className={styles.headerMenuLink}>Le musée</Link></li>
+                <li className={styles.headerMenuLi}><Link href={`#`} title={``} className={styles.headerMenuLink}>Librarie</Link></li>
+                <li className={styles.headerMenuLi}><Link href={`#`} title={``} className={styles.headerMenuLink}>Accès</Link></li>
+                <li className={styles.headerMenuLi}><Link href={`#`} title={``} className={styles.headerMenuLink}>Contact</Link></li>
+              </ul>
+            </nav>
           </div>
         </Container>
     </header>
--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import Container from '@/components/Container'
 import styles from './Intro.module.scss'
 
 const Intro = () => {
   return (
     <section className={styles.intro}>
-        
+        <Container className={styles.container}>
+          <h2 className={styles.introTitle}>Bienvenue !</h2>
+          {/* <svg className={styles.introTitle} width="560" height="120" viewBox="0 0 560 120">
+            <defs>
+              <path id="textPath" d="M10 130 C190 60 370 60 550 130"/>
+            </defs>
+            <text fill="black">
+              <textPath href="#textPath" startOffset="50%" textAnchor="middle">Bienvenue !</textPath>
+            </text>
+          </svg> */}
+
+          <p className={styles.introSubtitle}>
+            <span className={styles.introSpan}>Musée - Librairie - Café</span><br/>
+            Ville de Tuan, la capitale fictive
+          </p>
+        </Container>
     </section>
   )
 }
